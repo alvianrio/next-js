d517d11530ade7ca0b2d570386d6267b778e9f36
Rio Alvian <m.rioalvian@gmail.com>
2021-02-03 20:30:19 +0700

    Update index.tsx

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,7 +2,8 @@ import {container, title} from './styles'
 
 const Header = () => (
     <header css={container}>
-        <h1 css={title}>Welcome to Next.js! ABC</h1>
+        <h1 css={title}>Welcome to Next.js! ABC 12345</h1>
+        <p>Testing 1</p>
     </header>
 )
 
