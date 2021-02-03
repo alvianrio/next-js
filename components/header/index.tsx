60ab57468b2f1adc93503f802368f3a5e2990617
Rio Alvian <m.rioalvian@gmail.com>
2021-02-03 20:32:01 +0700

    berhasil

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,8 +2,7 @@ import {container, title} from './styles'
 
 const Header = () => (
     <header css={container}>
-        <h1 css={title}>Welcome to Next.js! ABC 12345</h1>
-        <p>Testing 1</p>
+        <h1 css={title}>Welcome to Next.js! ABC alhamdulillah berhasil</h1>
     </header>
 )
 
